20bbbde065dc9c6259be449bddf98b67a6f5b88a
yossi lerer <ylerer770@gmail.com>
2026-02-17 15:46:24 +0200

    finish all api

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from 'cors'
+import 'dotenv/config'
+import apiRouter from './routers/api.js'
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001
+
+app.use('/api', apiRouter)
+
+app.get('/', (req,res) => {
+    res.send("hello from router")
+    console.log("hello from router");
+    console.log(process.env.URI);
+    
+})
+
+
+app.listen(PORT, ()=> {
+    console.log(`http://localhost:${PORT}`);
+})
---
+++ server/routers/api.js
@@ -0,0 +1,56 @@
+import express from 'express'
+import { connectMongo } from '../DB/connect.js'
+import { createToken, tokenExtractor } from '../utils/jwt.js'
+
+ const apiRouter = express();
+
+ export const db = await connectMongo({
+    uri: process.env.URI,
+    dbName: process.env.DBNAME
+});
+
+
+apiRouter.post('/complaints', async (req, res) => {
+    const { category, message } = req.body;
+    const date = new Date().toLocaleDateString()
+    const result = await db .collection('messages').insertOne({
+            category, 
+            message,
+            createdAt: date
+        })
+        res.status(201).json({message: "The save was successful.",
+             obj : {
+                category,
+                message,
+                createdAt: date
+                }
+            }
+        )
+    }
+);
+
+
+apiRouter.post('/admin/login', async (req,res) => {
+    const { password } = req.body;
+    if (password === process.env.ADMIN_PASSWORD){
+        const token = createToken({password})
+         return res.status(200).json({message: 'Verification was successful', 
+            token
+        })
+    }
+    return res.status(401).json({error: "Unauthorized"})
+});
+
+
+apiRouter.get('/complaints', tokenExtractor, async (req,res) => {
+    try{ 
+    const data = await db.collection('messages').find({}).sort({"createdAt" : 1}).toArray()
+        console.log(data);
+    return res.status(200).json(data)
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+
+ export default apiRouter
